2559e6455be4f8a274283ebea97f06e96d08155a
Mayed Abdalla <mayed@Mayeds-MacBook-Pro.local>
2022-12-09 14:33:51 +0400

    Change font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
+import type { AppProps } from 'next/app'
+import {Noto_Naskh_Arabic } from '@next/font/google'
+
+const inter = Noto_Naskh_Arabic({  })
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return     <main className={inter.className}>
+  <Component {...pageProps} />
+</main>
 }
